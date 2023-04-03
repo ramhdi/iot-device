cec7acaf590f7803c563e070a694741fa0dfd10a
Rama Rahardi <rama.orion@gmail.com>
2023-04-03 10:34:19 +0700

    Init FSM implementation

--- src/main.rs
+++ src/main.rs
@@ -12,17 +12,25 @@ use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, alway
 use log::{error, info, warn};
 use std::{thread::sleep, time::Duration};
 
+enum State {
+    ConnWifi,
+    ConnMqtt,
+    Ok,
+}
+
 fn main() -> Result<()> {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_sys::link_patches();
 
-    println!("Entered Main function!");
+    println!("Starting app");
+
+    let mut state: State;
 
     // Init peripherals, sys_loop, nvs
     let peripherals = Peripherals::take().unwrap();
-    let sys_loop = EspSystemEventLoop::take().unwrap();
-    let nvs = EspDefaultNvsPartition::take().unwrap();
+    let sys_loop = EspSystemEventLoop::take()?;
+    let nvs = EspDefaultNvsPartition::take()?;
 
     // Init wifi driver
     let mut wifi_driver = EspWifi::new(peripherals.modem, sys_loop, Some(nvs))?;
@@ -30,23 +38,14 @@ fn main() -> Result<()> {
     const SSID: &str = "***";
     const PASSWORD: &str = "***";
 
+    state = State::ConnWifi;
+
     wifi_driver.set_configuration(&Configuration::Client(ClientConfiguration {
         ssid: SSID.into(),
         password: PASSWORD.into(),
         ..Default::default()
     }))?;
-
-    // Connect to wifi
-    wifi_driver.start().unwrap();
-    wifi_driver.connect().unwrap();
-    while !wifi_driver.is_connected()? {
-        // Try to connect until success
-        let config = wifi_driver.get_configuration()?;
-        println!("Waiting for AP: {:?}", config);
-        sleep(Duration::from_secs(1));
-    }
-    println!("Connected!");
-    println!("IP info: {:?}", wifi_driver.sta_netif().get_ip_info()?);
+    wifi_driver.start()?;
 
     // Init MQTT client
     let mqtt_config = MqttClientConfiguration::default();
@@ -64,13 +63,45 @@ fn main() -> Result<()> {
     let mut i = 0;
 
     loop {
-        // Publish to topic 'test/' every 10 seconds
-        i += 1;
-        let msg = format!("Hello MQTT! i = {}", i);
-        payload = msg.as_bytes();
-        mqtt_client.publish(topic, qos, retain, payload)?;
-        println!("Sent message to MQTT");
-        sleep(Duration::from_secs(10));
+        match state {
+            State::ConnWifi => {
+                // Connect to wifi
+                while !wifi_driver.is_connected()? {
+                    // Try to connect until success
+                    wifi_driver.connect()?;
+                    let config = wifi_driver.get_configuration()?;
+                    println!("Waiting for AP: {:?}", config);
+                    sleep(Duration::from_secs(1));
+                }
+                println!("Connected to AP!");
+                println!("IP info: {:?}", wifi_driver.sta_netif().get_ip_info()?);
+
+                state = State::ConnMqtt;
+            }
+            State::ConnMqtt => {
+                // mqtt_client = EspMqttClient::new(broker_url, &mqtt_config, move |message_event| {
+                //     warn!("Received from MQTT: {:?}", message_event)
+                // })?;
+                println!("Connected to MQTT Broker!");
+
+                state = State::Ok;
+            }
+            State::Ok => {
+                // Check wifi connection
+                if !wifi_driver.is_connected()? {
+                    println!("Disconnected from AP!");
+                    state = State::ConnWifi;
+                } else {
+                    // Publish to topic 'test/' every 10 seconds
+                    i += 1;
+                    let msg = format!("Hello MQTT! i = {}", i);
+                    payload = msg.as_bytes();
+                    mqtt_client.publish(topic, qos, retain, payload)?;
+                    println!("Sent message to MQTT");
+                    sleep(Duration::from_secs(10));
+                }
+            }
+        }
     }
 
     // Ok(())
